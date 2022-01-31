abc797967f00ad3564895538d52537dbde0d337b
nodeAriful <ariful.node82@gmail.com>
2022-02-01 00:29:57 +0600

    variable dicaration

---
+++ variable.js
@@ -0,0 +1,7 @@
+var name = "Ariful Howlader";
+var age = 23;
+
+console.log(name);
+console.log(age);
+console.log(name + " Kowns Javascript and javascript framework");
+console.log("He is Only " + age + " Years Old");
