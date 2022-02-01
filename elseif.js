eb27aac39f6f2370b1748d6d8a27273630e19ce3
nodeAriful <ariful.node82@gmail.com>
2022-02-01 22:23:15 +0600

    else if conditon

---
+++ elseif.js
@@ -0,0 +1,23 @@
+var a = 10;
+var b = 20;
+
+
+if(a>b){
+    console.log('A is gretter than B');
+}else if(a<b){
+    console.log('B is gretter than A');
+}
+else{
+    console.log('they Bothe are same');
+}
+
+
+var n = 2;
+
+if(n === 0){
+    console.log(n+ " is Zero");
+}else if(n%2 === 0){
+    console.log(n+ " is even number");
+}else{
+    console.log(n+ " Is odd number");
+}
