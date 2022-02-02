3bfd71cbf45284d7cfabcab5a7b297f41a75b79c
nodeAriful <ariful.node82@gmail.com>
2022-02-02 23:04:20 +0600

    escape notations

---
+++ escapeNotation.js
@@ -0,0 +1,10 @@
+// ***** Escape Notation
+
+
+var str = 'This is a \'String\'';
+
+var str2 = 'My love Banglades \nMuslime';
+var str3 = 'My love Banglades \tMuslime';
+var str4 = 'My love Banglades \\Muslime';
+
+console.log(str4);
