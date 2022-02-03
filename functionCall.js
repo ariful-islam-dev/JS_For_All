2e458607a2d5484849f62fd2af29865ac3d8798d
nodeAriful <ariful.node82@gmail.com>
2022-02-03 22:42:26 +0600

    function call

---
+++ functionCall.js
@@ -0,0 +1,24 @@
+function functionName (){
+    console.log("My Name is Ariful");
+}
+// functionName()
+
+
+function add() {
+    var a = 10;
+    var b = 20;
+    console.log(a+b);
+}
+add()
+
+function sub() {
+    var a = 50;
+    var b = 20;
+    console.log(a-b);
+}
+
+sub()
+
+for(var i = 0; i<10; i++){
+    functionName()
+}
